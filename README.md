278e2f47b41790125cbb722d6f9cc7e1f73e3a9f
Prashant Baid <prashantvbaid@gmail.com>
2016-02-10 19:17:55 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # npm-username
-Check the availability of npm username
+Check the availability of npm username when doing ```npm adduser```
+
+
+## Install
+
+```
+$ npm install npm-username -g
+```
+
+## Usage
+
+```
+$ npm-username '<username>'
+```
+
+## Example
+```
+$ npm-username 'jsmith'
+$ The username already exists :(
+```
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+#!/usr/bin/env node
+
 var request = require('request');
 
-if(process.argv[2]) {
-  checkCredentials(process.argv[2]);
-}
-else {
-  console.log('Please enter a name.');
-  process.exit(0);
+if (process.argv[2]) {
+    checkCredentials(process.argv[2]);
+} else {
+    console.log('Please enter a name.');
+    process.exit(0);
 }
 
 
 function checkCredentials(username) {
-  var req = {
-    url : 'https://www.npmjs.com/~'+username
-  }
-  request(req, function(err, resp, body) {
-    if(err)
-      console.log('Some error occured in searching your username.');
-    else {
-      if(resp.statusCode === 200)
-        console.log('The username already exists :(');
-      else if(resp.statusCode === 404 || resp.statusCode === 500)
-        console.log('The username is available :)');
-      else
-        console.log('Existence of the username could not be determined by this module. Sorry :(')
+    var req = {
+        url: 'https://www.npmjs.com/~' + username
     }
-  });
+    request(req, function(err, resp, body) {
+        if (err)
+            console.log('Some error occured in searching your username.');
+        else {
+            if (resp.statusCode === 200)
+                console.log('The username already exists :(');
+            else if (resp.statusCode === 404 || resp.statusCode === 500)
+                console.log('The username is available :)');
+            else
+                console.log('Existence of the username could not be determined by this module. Sorry :(')
+        }
+    });
 }
